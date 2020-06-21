c6c7b4775be2e0a0836de987534e00421bc0f4a9
Roxiun <ronan.naidu@hotmail.com>
2020-06-21 18:54:16 +0800

    Commit Files - 10

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,12 @@
 function init() {
     document.getElementById("iconp");
+    if (size == 10){
+        {document.getElementById("iconp").style.webkitTransform = 'scale(0.1)';
+    }
+    if (size == 5){
+        {document.getElementById("iconp").style.webkitTransform = 'scale(0.05)';
+    }
+    if (size == 2){
+        {document.getElementById("iconp").style.webkitTransform = 'scale(0.02)';
+    }
 }
